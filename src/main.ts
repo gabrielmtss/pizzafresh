5d04b1412e5d3c3b2dee40712a0ca7d7c0c29169
Gabriel Matos <g.matos@outlook.com>
2022-05-18 22:27:54 -0300

    config swagger and class-validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // configuração do class-validator e do class-transformer
+  app.useGlobalPipes(new ValidationPipe());
+
+  // configuração do swagger
+  const config = new DocumentBuilder()
+    .setTitle('Pizza Fresh')
+    .setDescription('Aplicação para gestão das mesas de uma pizzaria')
+    .setVersion('1.0.0')
+    .addTag('status')
+    .addTag('table')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3333);
 }
 bootstrap();
--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,3 +1,12 @@
+import { IsNumber, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateTableDto {
+  @IsPositive()
+  @IsNumber()
+  @ApiProperty({
+    description: 'O número da mesa',
+    example: 1,
+  })
   number: number;
 }
