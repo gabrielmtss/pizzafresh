d32cb50249a930a9b42673127023ec0ded6aac31
Gabriel Matos <g.matos@outlook.com>
2022-05-24 20:30:59 -0300

    implement methods

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,21 +1,63 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { TableService } from './table.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateTableDto } from './dto/create-table.dto';
+import { UpdateTableDto } from './dto/update-table.dto';
 import { Table } from './entities/table.entity';
-import { ApiTags } from '@nestjs/swagger';
+import { TableService } from './table.service';
 
 @ApiTags('table')
 @Controller('table')
 export class TableController {
-  constructor(private tableService: TableService) {}
+  constructor(private readonly tableService: TableService) {}
 
   @Get()
-  findAll(): Table[] {
+  @ApiOperation({
+    summary: 'Listar todas as mesas',
+  })
+  findAll(): Promise<Table[]> {
     return this.tableService.findAll();
   }
 
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Visualizar uma mesa',
+  })
+  findOne(@Param('id') id: string): Promise<Table> {
+    return this.tableService.findOne(id);
+  }
+
   @Post()
-  create(@Body() createTableDto: CreateTableDto): Table {
-    return this.tableService.create(createTableDto);
+  @ApiOperation({
+    summary: 'Criar uma mesa',
+  })
+  create(@Body() dto: CreateTableDto): Promise<Table> {
+    return this.tableService.create(dto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({
+    summary: 'Editar uma mesa pelo ID',
+  })
+  update(@Param('id') id: string, @Body() dto: UpdateTableDto): Promise<Table> {
+    return this.tableService.update(id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({
+    summary: 'Remover uma mesa pelo ID',
+  })
+  delete(@Param('id') id: string) {
+    this.tableService.delete(id);
   }
 }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,20 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTableDto } from './dto/create-table.dto';
 import { Table } from './entities/table.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateTableDto } from './dto/update-table.dto';
 
 @Injectable()
 
 // classe: possui atributos e métodos
 export class TableService {
-  tables: Table[] = [];
+  constructor(private readonly prisma: PrismaService) {} // Chamada ao prisma
 
-  findAll(): Table[] {
-    return this.tables;
+  findAll(): Promise<Table[]> {
+    return this.prisma.table.findMany();
   }
 
-  create(createTableDto: CreateTableDto): Table {
-    const table: Table = { id: 'id_aleatorio', ...createTableDto };
-    this.tables.push(table);
-    return table;
+  async findById(id: string): Promise<Table> {
+    const record = await this.prisma.table.findUnique({ where: { id } });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o ID '${id}' não encontrado.`);
+    }
+
+    return record;
+  }
+
+  async findOne(id: string): Promise<Table> {
+    return this.findById(id);
+  }
+
+  create(dto: CreateTableDto): Promise<Table> {
+    const data: Table = { ...dto };
+
+    return this.prisma.table.create({ data }).catch(this.handleError);
+  }
+
+  handleError(error: Error) {
+    console.log(error.message);
+
+    return undefined;
+  }
+
+  async update(id: string, dto: UpdateTableDto): Promise<Table> {
+    await this.findById(id);
+
+    const data: Partial<Table> = { ...dto };
+
+    return this.prisma.table.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async delete(id: string) {
+    await this.findById(id);
+
+    await this.prisma.table.delete({ where: { id } });
   }
 }
